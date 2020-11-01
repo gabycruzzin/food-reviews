720c99cb7aa0a0d5bbe844c11744661d0067d39d
Gaby Cruz <gcruz.a10@gmail.com>
2020-11-01 10:27:07 -0600

    new file

---
+++ src/apis/queries.js
@@ -0,0 +1,19 @@
+var dice = 3;
+var sides = 6;
+var query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+fetch('/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: { dice, sides },
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));

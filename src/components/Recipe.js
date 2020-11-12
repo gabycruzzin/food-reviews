952779d06322a1847f2eabe5e44101e7b6332ce3
Gaby Cruz <gcruz.a10@gmail.com>
2020-11-12 16:39:12 -0600

    cleaning up

---
+++ src/components/Recipe.js
@@ -0,0 +1,9 @@
+import { Typography } from "@material-ui/core";
+
+export const Recipe = () => {
+  return (
+    <Typography variant="h6" gutterBottom>
+      Recipe
+    </Typography>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -3,23 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "fontsource-roboto";
-import { ApolloProvider } from "@apollo/client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
 
 Amplify.configure(config);
 
-const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
-  cache: new InMemoryCache(),
-});
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );

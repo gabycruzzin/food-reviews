56e8b6aa877d14a3f2b1165923e852937a587b45
Gaby Cruz <gcruz.a10@gmail.com>
2020-11-14 14:01:14 -0600

    1st version of working drawer

--- src/App.js
+++ src/App.js
@@ -10,15 +10,31 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { Button, IconButton, TextField, Typography } from "@material-ui/core";
 import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
 
 const initialFormState = { name: "", description: "" };
+const drawerWidth = 400;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: "flex",
   },
   componentContainer: {
     width: "100%",
+  },
+  appBar: {
+    backgroundColor: "transparent",
+    color: "inherit",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
   },
   paper: {
     padding: theme.spacing(2),
@@ -28,12 +44,33 @@ const useStyles = makeStyles((theme) => ({
   input: {
     display: "none",
   },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  recipeContainer: {
+    display: "grid",
+    gridTemplateColumns: 300,
+  },
 }));
 
-export const App = () => {
+export const App = (props) => {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
   const classes = useStyles();
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   useEffect(() => {
     fetchNotes();
@@ -85,81 +122,47 @@ export const App = () => {
     });
   }
 
-  return (
-    <div className={classes.root}>
-      <Typography variant="h2" gutterBottom>
-        My Recipes
-      </Typography>
-      <Grid container className={classes.componentContainer} spacing={3}>
-        {notes.map((note) => (
-          <Grid item xs={12} sm={6} md={3} lg={3} key={note.id || note.name}>
-            <Paper className={classes.paper}>
-              <Typography variant="h4">{note.name}</Typography>
-              <Typography variant="body1" style={{ whiteSpace: "pre-line" }}>
-                {note.description}
-              </Typography>
-              {note.image && (
-                <img
-                  src={note.image}
-                  alt=""
-                  style={{ width: "100%", height: "auto" }}
-                />
-              )}
-              <Button
-                variant="outlined"
-                size="small"
-                disableElevation
-                onClick={() => deleteNote(note)}
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" className={classes.recipeContainer}>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          value={formData.name}
+        />
+        <TextField
+          id="standard-multiline-static"
+          variant="outlined"
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+          label="Recipe"
+          multiline
+          rows={6}
+          value={formData.description}
+        />
+        <Grid container justify="space-between">
+          <div>
+            <input
+              accept="image/*"
+              className={classes.input}
+              id="icon-button-file"
+              type="file"
+              onChange={onUpload}
+            />
+            <label htmlFor="icon-button-file">
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
               >
-                Delete note
-              </Button>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
-      <Grid
-        container
-        justify="center"
-        className={classes.componentContainer}
-        spacing={3}
-      >
-        <Grid item xs={4}>
-          <TextField
-            id="outlined-basic"
-            label="Name"
-            variant="outlined"
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            value={formData.name}
-          />
-          <br />
-          <TextField
-            id="standard-multiline-static"
-            variant="outlined"
-            onChange={(e) =>
-              setFormData({ ...formData, description: e.target.value })
-            }
-            label="Recipe"
-            multiline
-            rows={6}
-            value={formData.description}
-          />
-          <br />
-          <input
-            accept="image/*"
-            className={classes.input}
-            id="icon-button-file"
-            type="file"
-            onChange={onUpload}
-          />
-          <label htmlFor="icon-button-file">
-            <IconButton
-              color="primary"
-              aria-label="upload picture"
-              component="span"
-            >
-              <PhotoCamera />
-            </IconButton>
-          </label>
+                <PhotoCamera />
+              </IconButton>
+            </label>
+          </div>
           <Button
             onClick={createNote}
             variant="contained"
@@ -170,6 +173,83 @@ export const App = () => {
           </Button>
         </Grid>
       </Grid>
+    </div>
+  );
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar} elevation={0}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h2">My Recipes</Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Hidden mdUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={"bottom"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            anchor={"right"}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+
+        <Grid container className={classes.componentContainer} spacing={3}>
+          {notes.map((note) => (
+            <Grid item xs={12} sm={6} md={3} lg={3} key={note.id || note.name}>
+              <Paper className={classes.paper}>
+                <Typography variant="h4">{note.name}</Typography>
+                <Typography variant="body1" style={{ whiteSpace: "pre-line" }}>
+                  {note.description}
+                </Typography>
+                {note.image && (
+                  <img
+                    src={note.image}
+                    alt=""
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                )}
+                <Button
+                  variant="outlined"
+                  size="small"
+                  disableElevation
+                  onClick={() => deleteNote(note)}
+                >
+                  Delete note
+                </Button>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </main>
     </div>
   );
 };
